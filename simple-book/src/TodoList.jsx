91f1285d0c119ddc5144b8d4a65f04f6653d64ce
雾山 <li997477295@outlook.com>
2019-03-14 15:41:24 +0800

    redux实现todolist

--- simple-book/src/TodoList.jsx
+++ simple-book/src/TodoList.jsx
@@ -1,138 +1,55 @@
-import React, { Fragment } from 'react'
-import axios from 'axios'
-import Mock from 'mockjs'
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+import store from './store'
 
-import TodoItem from './TodoItem'
-import './style.css'
-
-class TodoList extends React.Component {
+class TodoList extends Component {
   constructor(props) {
     super(props)
-    // 当组件的state或者props变化时，render函数就会重新执行
-    this.state = {
-      inputValue: '',
-      list: []
-    }
+    this.state = store.getState()
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleButtonClick = this.handleButtonClick.bind(this)
-    this.handleDeleteItem = this.handleDeleteItem.bind(this)
-  }
-  // 在组件即将挂载到页面的时刻自动执行
-  componentWillMount() {
-    console.log('componentWillMount')
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    store.subscribe(this.handleStoreChange)
   }
   render() {
-    console.log('parent render')
     return (
-      <Fragment>
-        <div>
-          {/* 多行注释 */}
-          {
-            // 单行注释
-          }
-          <label htmlFor="insertArea">输入内容</label>
-          <input
-            id="insertArea"
-            className="input"
-            type="text"
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
-            ref={input => {
-              this.input = input
-            }}
-          />
-          <button onClick={this.handleButtonClick}>提交</button>
-        </div>
-        <ul
-          ref={ul => {
-            this.ul = ul
-          }}
-        >
-          {this.getTodoItem()}
-        </ul>
-      </Fragment>
+      <div style={{ margin: '10px' }}>
+        <Input
+          placeholder="todo info"
+          style={{ width: '300px', marginRight: '10px' }}
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        <Button type="primary" onClick={this.handleButtonClick}>
+          提交
+        </Button>
+        <List
+          style={{ width: '300px', marginTop: '10px' }}
+          size="small"
+          bordered
+          dataSource={this.state.list}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+      </div>
     )
-  }
-  // 页面被挂载到页面后，自动执行
-  componentDidMount() {
-    // console.log('componentDidMount')
-    var arr = ['momo', 'yanzi', 'ziweie']
-
-    Mock.mock('/api/todolist', arr)
-    // ajax请求习惯放在这儿
-    axios
-      .get('/api/todolist')
-      .then(res => {
-        console.log(res.data)
-        this.setState(() => ({ list: [...res.data] }))
-      })
-      .catch(() => {
-        console.log('error')
-      })
-  }
-  // 组件被更新之前，会被自动执行
-  shouldComponentUpdate() {
-    // console.log('shouldComponentUpdate')
-    return true
-  }
-  // 组件被更新之前，会被自动执行，但他在shouldComponentUpdate之后执行
-  // 如果shouldComponentUpdate返回true，才执行。false则不执行
-  componentWillUpdate() {
-    // console.log('componentWillUpdate')
-  }
-  // 组件更新完成之后会被执行
-  componentDidUpdate() {
-    // console.log('componentDidUpdate')
-  }
-  componentWillReceiveProps() {
-    // console.log('parent componentWillReceiveProps')
-  }
-  getTodoItem() {
-    return this.state.list.map((item, index) => {
-      return (
-        <li key={item}>
-          <TodoItem
-            content={item}
-            index={index}
-            deleteItem={this.handleDeleteItem}
-          />
-          {/* 
-            dangerouslySetInnerHTML: 不转义
-            <li
-              key={index}
-              onClick={this.handleDeleteItem.bind(this, index)}
-              dangerouslySetInnerHTML={{ __html: item }}
-            /> 
-          */}
-        </li>
-      )
-    })
   }
   handleInputChange(e) {
-    // const value = e.target.value
-    const value = this.input.value
-    this.setState(() => ({
-      inputValue: value
-    }))
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action)
+  }
+  handleStoreChange() {
+    this.setState(store.getState())
   }
   handleButtonClick() {
-    // 因为setState函数是异步的
-    this.setState(
-      prevState => ({
-        list: [...prevState.list, prevState.inputValue],
-        inputValue: ''
-      }),
-      () => {
-        console.log(this.ul.querySelectorAll('li').length)
-      }
-    )
-  }
-  handleDeleteItem(index) {
-    this.setState(prevState => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return { list }
-    })
+    const action = {
+      type: 'add_todo_item',
+      value: this.state.inputValue
+    }
+    store.dispatch(action)
   }
 }
 
