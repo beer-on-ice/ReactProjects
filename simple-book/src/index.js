025566048ccc43bb3725aba3d1f6ce0e87bb3dac
雾山 <li997477295@outlook.com>
2019-03-18 16:03:22 +0800

    初始化

--- simple-book/src/index.js
+++ simple-book/src/index.js
@@ -1,12 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import TodoList from './TodoList'
-import { Provider } from 'react-redux'
-import store from './store'
 
-const App = (
-  <Provider store={store}>
-    <TodoList />
-  </Provider>
-)
-ReactDOM.render(App, document.getElementById('root'))
+ReactDOM.render(<div />, document.getElementById('root'))
--- simple-book/src/store/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const CHANGE_INPUT_VALUE = 'change_input_value'
-
-export const ADD_NEW_ITEM = 'add_new_item'
-
-export const DELETE_ITEM = 'delete_item'
