3a3e6c0da2b019cde05b6b71d2947d714d3c6f68
雾山 <li997477295@outlook.com>
2019-03-18 15:34:41 +0800

    react-redux使用

--- simple-book/src/TodoList.jsx
+++ simple-book/src/TodoList.jsx
@@ -1,8 +1,64 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+import {
+  changeInputValue,
+  addNewItem,
+  deleteItem
+} from './store/actionCreators'
 
-class TodoList extends Component {
-  render() {
-    return
+const TodoList = props => {
+  const {
+    inputValue,
+    changeInputValue,
+    handleClick,
+    list,
+    handleDelete
+  } = props
+  return (
+    <div>
+      <div>
+        <input type="text" value={inputValue} onChange={changeInputValue} />
+        <button onClick={handleClick}>提交</button>
+      </div>
+      <ul>
+        {list.map((item, index) => {
+          return (
+            <li key={index} onClick={() => handleDelete(index)}>
+              {item}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    inputValue: state.inputValue,
+    list: state.list
   }
 }
-export default TodoList
+
+// store.dispatch / props
+const mapDispatchToProps = dispatch => {
+  return {
+    changeInputValue(e) {
+      const action = changeInputValue(e.target.value)
+      dispatch(action)
+    },
+    handleClick() {
+      const action = addNewItem()
+      dispatch(action)
+    },
+    handleDelete(index) {
+      const action = deleteItem(index)
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
---
+++ simple-book/src/store/actionTypes.js
@@ -0,0 +1,5 @@
+export const CHANGE_INPUT_VALUE = 'change_input_value'
+
+export const ADD_NEW_ITEM = 'add_new_item'
+
+export const DELETE_ITEM = 'delete_item'
