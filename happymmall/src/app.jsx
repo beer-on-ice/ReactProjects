9ccd9b268309503bee7f60a85569884122bff7e6
雾山 <li997477295@outlook.com>
2019-03-29 16:31:24 +0800

    webpack3配置

---
+++ happymmall/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.scss'
+
+ReactDOM.render(
+  <div>
+    <h1>hello world</h1>
+  </div>,
+  document.getElementById('app')
+)
---
+++ happymmall/webpack.config.js
@@ -0,0 +1,91 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'js/[name]-bundle-[chunkhash:5].js'
+  },
+  module: {
+    rules: [
+      // react语法文件处理
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      // css文件处理
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      // scss文件处理
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      // 图片处理
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name]-[hash:5].[ext]',
+            limit: 8197,
+            outputPath: 'statics/imgs/'
+          }
+        }
+      },
+      // 字体图标处理
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name]-[hash:5].[ext]',
+            limit: 8197,
+            outputPath: 'statics/fonts/'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    // 处理html文件
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    // 独立css文件
+    new ExtractTextPlugin({
+      filename: 'css/[name]-bundle-[chunkhash:5].css'
+    }),
+    // 提出公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/base-[chunkhash:5].js'
+    })
+  ],
+  devServer: {
+    port: 8081,
+    overlay: true
+  }
+}
