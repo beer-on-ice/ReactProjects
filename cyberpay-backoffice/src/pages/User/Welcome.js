609cba8e54d27602c9bbfaf649f2e4b2f5026f70
雾山 <li997477295@outlook.com>
2019-05-27 14:06:01 +0800

    删除无用

--- cyberpay-backoffice/src/pages/User/Welcome.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const WelcomePage = () => <div>欢迎登入后台管理系统！</div>
-
-export default WelcomePage
--- cyberpay-backoffice/src/services/menu.js
+++
@@ -1,6 +0,0 @@
-import axios from '@/utils/axios'
-
-// 获取菜单列表
-export default async function getMenuList(params) {
-  return axios.post('/server/menu/list', { ...params })
-}
